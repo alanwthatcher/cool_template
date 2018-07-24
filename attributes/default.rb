486041305c6724ec6fa1ebd9be6a0e19bd94faca
Alan Thatcher <alanwthatcher@gmail.com>
2018-07-24 17:26:52 -0500

    the things

---
+++ attributes/default.rb
@@ -0,0 +1,15 @@
+default['cool_template']['my_app'] = 'app3'
+
+default['cool_template']['app1']['key1'] = 'app1value1'
+default['cool_template']['app1']['key2'] = 'app1value2'
+default['cool_template']['app1']['key3'] = 'app1value3'
+default['cool_template']['app1']['key4'] = 'app1value4'
+
+default['cool_template']['app2']['key1'] = 'app2value1'
+default['cool_template']['app2']['key2'] = 'app2value2'
+default['cool_template']['app2']['key3'] = 'app2value3'
+
+default['cool_template']['app3']['key1'] = 'app3value1'
+default['cool_template']['app3']['key2'] = 'app3value2'
+default['cool_template']['app3']['key3'] = 'app3value3'
+default['cool_template']['app3']['key4'] = 'app3value4'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+who_am_i = node['cool_template']['my_app']
+
+template '/tmp/cool' do
+  source 'cool.erb'
+  variables key_values: node['cool_template'][who_am_i]
+end
