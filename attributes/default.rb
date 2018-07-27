034516a48e8be313fb48dd1e7b691ed2584e6a2c
Alan Thatcher <alanwthatcher@gmail.com>
2018-07-26 23:16:58 -0500

    changes and fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 ### HASH BASED ###
 # Identify the application type being referenced
-default['cool_template']['my_app'] = 'app1'
+default['cool_template']['my_app'] = 'app2'
 
 # Hash for a thing called app1
 default['cool_template']['app1']['key1'] = 'app1value1'
@@ -8,14 +8,28 @@ default['cool_template']['app1']['key2'] = 'app1value2'
 default['cool_template']['app1']['key3'] = 'app1value3'
 default['cool_template']['app1']['key4'] = 'app1value4'
 default['cool_template']['app1']['key5'] = 'app1value5'
+default['cool_template']['app1']['header1'] = {
+  SomeConfigThing1: "ValueOfConfigThing1",
+  SomeConfigThing2: "ValueOfConfigThing2",
+  SomeConfigThing3: "ValueOfConfigThing3",
+}
 
 # Hash for a thing called app2
 default['cool_template']['app2']['key1'] = 'app2value1'
 default['cool_template']['app2']['key2'] = 'app2value2'
+default['cool_template']['app2']['header1'] = {
+  SomeConfigThing1: "ValueOfConfigThing1",
+  SomeConfigThing2: "ValueOfConfigThing2",
+  SomeConfigThing3: "ValueOfConfigThing3",
+  SomeConfigThing4: "ValueOfConfigThing4",
+}
 default['cool_template']['app2']['key3'] = 'app2value3'
-default['cool_template']['app2']['proxy_pass'] = ['string1', 'string2']
 
 # Hash for a thing called app3
+default['cool_template']['app3']['header1'] = {
+  SomeConfigThing1: "ValueOfConfigThing1",
+  SomeConfigThing2: "ValueOfConfigThing2",
+}
 default['cool_template']['app3']['key1'] = 'app3value1'
 default['cool_template']['app3']['key2'] = 'app3value2'
 ### \hash based ###
